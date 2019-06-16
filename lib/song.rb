42b9d394f48776ff84705386bb9fae5be099d29e
Oana Berentan <oana.berentan@yahoo.com>
2019-06-16 11:57:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,13 @@
 class Song
-  attr_accessor :name, :artist
+  attr_accessor :name, :artist, :tile
 
 def initialize(name)
   @name = name
 end
 
+def self.new_by_filename(filename)
+  song = self.new
+  song.
+end
 
 end
