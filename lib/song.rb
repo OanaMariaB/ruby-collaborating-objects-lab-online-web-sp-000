4eaf5c1f2b68ecde39b0dcbe8d5eb28383a97b2f
Oana Berentan <oana.berentan@yahoo.com>
2019-06-16 12:24:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,5 +5,12 @@ def initialize(name)
   @name = name
 end
 
+def self.new_by_filename(file_name)
+    song = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
 
 end
